5f546f078b1965b1db9f64d7ccedc3c7b63a042e
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-21 20:22:00 +0300

    cucumber

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -44,5 +45,10 @@ public class Login_StepDefinitions {
     public void userEntersAdminPassword() {
         System.out.println("User enters admin password");
 
+    }
+
+    @Given("user is on the library login page")
+    public void userIsOnTheLibraryLoginPage() {
+        System.out.println("User is on the library login page");
     }
 }
